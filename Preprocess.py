eef64421f64f03c86746d36caf859fa2d7268572
junghun.kim <junghunkim@kaist.ac.kr>
2021-11-01 11:13:59 +0900

    Update Preprocess.py

--- Preprocess.py
+++ Preprocess.py
@@ -23,7 +23,7 @@ class Preprocess:
         print('Extraction starts')
         time=5500000000
 
-        os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="./rising-timing-327605-5d1392df9030.json"
+#         os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="google cloud key"   #put your own google cloud key.
         client = bigquery.Client()
         QUERY = (
              """
